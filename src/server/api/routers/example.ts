c89600b1cbcb480e830c34f7bba7856d560aa228
starling <starling@starlings-MBP.lan>
2023-04-01 17:48:51 -0500

    create & read implement

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -3,14 +3,21 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 
 export const exampleRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
+  create: publicProcedure
+    .input(
+      z.object({
+        content: z.string().min(2),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      return await ctx.prisma.example.create({
+        data: {
+          content: input.content,
+        },
+      });
     }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.example.findMany();
   }),
 });
